e52a7087dd270b14d9f5ab64969120ddb20c03ae
shreyasaha1997 <shreya@revoticengineering.com>
2018-11-25 11:25:51 +0530

    exe file

--- README.md
+++
@@ -1,2 +0,0 @@
-# ipfs-dynamic-hosting
-Dynamic Hosting Functionality by pinning the file to the IPFS Node running locally on user's device
--- desktop.py
+++ desktop.py
@@ -9,111 +9,112 @@ import gettext
 _ = gettext.gettext
 import logging
 from TraceConsole import *
+import time
 
 hashes = []
 local_files = []
 
 t = TraceConsole()
+time = time.time()
+name = str(time) + ".txt"
+f= open(name,"w+")
 
-
-
+logging.basicConfig(filename=name,
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
 
 
 def pins_from_database():
-    t.log('Ading everything from website to local repository')
+    t.log('Adding everything from website to local repository')
+    logging.info('Adding everything from website to local repository')
     r = requests.get(url="http://13.66.133.71:8000/jsony", headers={'Connection':'close'})
     data = r.json()
     data = data['array']
-    str = ""
+    str1 = ""
     for hash in data:
         if hash['ids'] not in hashes:
             status = os.system("ipfs pin add " + hash['ids'])
             hashes.append(hash['ids'])
             local_files.append(hash['ids'])
             if status != 0:
-                str = str + hash['ids'] + " " + "Not pinned yet \n"
+                str1 = str1 + hash['ids'] + " " + "Not pinned yet \n"
     top.after(20000, pins_from_database)
 
 def view():
     t.log('displaying files that are pinned to local node from website')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
-    str = ""
+    logging.info('displaying files that are pinned to local node from website')
+    str1 = ""
     for hash in hashes:
-        str = str + hash + "\n"
-    t.log(str)
-    if str == "":
+        str1 = str1 + hash + "\n"
+    t.log(str1)
+    logging.info(str1)
+    if str1 == "":
         L2.config(text="oops")
     else:
-        L2.config(text=str)
+        L2.config(text=str1)
 
 def view1():
     t.log('displaying files that are pinned to local node')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
-    str = ""
+    logging.info('displaying files that are pinned to local node')
+    str1 = ""
     for hash in local_files:
-        str = str + hash + "\n"
-    t.log(str)
-    if str == "":
+        str1 = str1 + hash + "\n"
+    t.log(str1)
+    logging.info(str1)
+    if str1 == "":
         L2.config(text="No file is there")
     else:
-        L2.config(text=str)
+        L2.config(text=str1)
 
 def pin():
     t.log('Pinning hash seperately')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
+    logging.info('Pinning hash seperately')
     hash = Entry.get(E1)
     print(hash)
     t.log(hash + ' to be pinned')
+    logging.info(hash + ' to be pinned')
     try:
         status = os.system("ipfs pin add " + hash)
         if hash not in local_files:
             local_files.append(hash)
         if status!=0:
             L4.config(text="Pinning not Successful")
-            t.log('Pinning is successful')
+            t.log('Pinning is not successful')
+            logging.info('Pinning is not successful')
         else:
             L4.config(text="Pinning Successful")
-            t.log('Pinning is not successful')
+            t.log('Pinning is successful')
+            logging.info('Pinning is successful')
     except tkinter.TclError:
         L4.config(text="Error in pinning")
 
 def unpin():
     t.log('Unpinning hash')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
+    logging.info('Unpinning hash')
     hash = Entry.get(E2)
     t.log(hash + ' to be unpinned')
+    logging.info(hash + ' to be unpinned')
     try:
         status = os.system("ipfs pin rm " + hash)
         if hash in local_files:
             local_files.remove(hash)
         if status!=0:
             L4.config(text="Unpinning not Successful")
-            t.log('Unpinning is successful')
+            t.log('Unpinning is not successful')
+            logging.info('Unpinning is not successful')
         else:
             L4.config(text="Unpinning Successful")
-            t.log('Unpinning is not successful')
+            t.log('Unpinning is successful')
+            logging.info('Unpinning is successful')
     except tkinter.TclError:
         L4.config(text="Error in unpinning")
 #
 def daemon():
     t.log('IPFS Daemon about to start')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
+    logging.info('IPFS Daemon about to start')
     os.system("ipfs daemon")
-
-
-
-def start_daemon():
-    t.log('hello world!')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
-    new_window = Toplevel(top)
-    SD = Button(new_window,text="start_daemon",command = start).grid(row=0,column=0)
-    L7 = Label(new_window, ).grid(row=1,column=0)
 
 
 # pin_all()
@@ -122,8 +123,7 @@ top = tkinter.Tk()
 
 def main():
     t.log('Main method starting ipfs daemon')
-    logger = logging.getLogger(__name__)
-    logger.info(_('Done!'))
+    logging.info('Main method starting ipfs daemon')
     multiprocessing.freeze_support()
     pool = Pool(processes=1)
     P1 = pool.apply_async(daemon())
@@ -152,6 +152,7 @@ B=Button(top, text ="View File Hashes",command = view1).grid(row=10,column=0)
 L3 = Label(top,)
 L3.grid(row=11,column=0)
 top.after(20000, pins_from_database)
+
 top.mainloop()
 
 
